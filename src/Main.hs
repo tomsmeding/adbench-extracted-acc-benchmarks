2eb0288cda492137e0eecc2a8bc5e93d1cd21680
Tom Smeding <tom.smeding@gmail.com>
2020-10-26 13:37:14 +0100

    Remove references to local directories, and -gpu

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE LambdaCase #-}
 module Main where
 
-import Control.Monad (forM, forM_)
+import Control.Monad (forM_)
 import qualified Criterion.Measurement as CM
 import qualified Criterion.Measurement.Types as CMT
+import System.Environment
 
-import Data.Array.Accelerate (Acc, Vector, Matrix)
+import Data.Array.Accelerate (Acc, Vector, Matrix, Arrays)
+import qualified Data.Array.Accelerate.Interpreter as I
 import qualified Data.Array.Accelerate.LLVM.Native as CPU
+import qualified Data.Array.Accelerate.LLVM.PTX as GPU
 
 import GMMIO
 import M1 (inputProgram)
@@ -25,15 +28,46 @@ data TimeResult = TimeResult { tmCompile :: Double
                              , tmCompute :: Double }
   deriving (Show)
 
-timeRun :: Program -> GMMIn -> IO TimeResult
-timeRun prog input = do
-    let compiled = CPU.run1 prog
+data Backend = Interpreter | CPU | GPU
+  deriving (Show)
+
+run1For :: (Arrays a, Arrays b) => Backend -> (Acc a -> Acc b) -> a -> b
+run1For Interpreter = I.run1
+run1For CPU = CPU.run1
+run1For GPU = GPU.run1
+
+timeRun :: Options -> Program -> GMMIn -> IO TimeResult
+timeRun options prog input = do
+    debugLn options "Compiling..."
+    let compiled = run1For (optBackend options) prog
+    debugLn options "Timing compilation force..."
     tm1 <- CMT.measTime . fst <$> CM.measure (CMT.whnf (`seq` ()) compiled) 1
+    debugLn options "Timing apply force..."
     tm2 <- CMT.measTime . fst <$> CM.measure (CMT.nf compiled input) 1
     return (TimeResult tm1 tm2)
 
+data Options = Options { optBackend :: Backend
+                       , optDebug :: Bool }
+  deriving (Show)
+
+defaultOptions :: Options
+defaultOptions = Options CPU False
+
+parseOption :: String -> Options -> Options
+parseOption "-inter" o = o { optBackend = Interpreter }
+parseOption "-cpu"   o = o { optBackend = CPU }
+parseOption "-gpu"   o = o { optBackend = GPU }
+parseOption "-debug" o = o { optDebug = True }
+parseOption opt      _ = error ("Unrecognised option '" ++ opt ++ "'")
+
+debugLn :: Options -> String -> IO ()
+debugLn (Options { optDebug = True }) = putStrLn
+debugLn _ = const (return ())
+
 main :: IO ()
 main = do
+    options <- foldr parseOption defaultOptions <$> getArgs
+
     let programs :: [Program]
         programs = 
             [inputProgram
@@ -51,5 +85,6 @@ main = do
     -- I've only tested with the 1k and 10k directories.
     input <- readInstance "gmm_1k_d32_K25.txt"
 
-    forM_ programs $ \prog ->
-        timeRun prog input >>= print
+    forM_ (zip [1::Int ..] programs) $ \(i, prog) -> do
+        debugLn options ("Running program " ++ show i)
+        timeRun options prog input >>= print
